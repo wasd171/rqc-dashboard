df8ac30142b9e9e82d629e5378de4d1568fc22c8
Konstantin Nesterov <wasd171@gmail.com>
2017-05-29 19:45:05 +0200

    Minimal working React + TS

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -1,1 +1,15 @@
-console.log('hey')
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+import App from './components/App'
+
+console.log('Hi')
+
+async function main() {
+    ReactDOM.render(
+        React.createElement(App), 
+        document.getElementById('app')
+    )
+}
+
+main()
