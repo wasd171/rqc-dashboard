0f159e7d1970bec5b22575114faf06e3107f5e57
Konstantin Nesterov <wasd171@gmail.com>
2017-05-30 14:33:49 +0200

    Moved providers from App to MetaProvider

--- src/renderer/components/App/index.tsx
+++ src/renderer/components/App/index.tsx
@@ -1,13 +1,6 @@
 import * as React from 'react'
 import styled from 'styled-components'
 
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-import getMuiTheme from 'material-ui/styles/getMuiTheme'
-
-import Lock from 'renderer/services/Lock'
-
-const muiTheme = getMuiTheme();
-
 const Container = styled.div`
     min-width: 100vw;
     max-width: 100vw;
@@ -16,17 +9,11 @@ const Container = styled.div`
 `
 
 class App extends React.Component<{}, {}> {
-    componentDidMount() {
-        Lock.emailcode((...args) => console.log(args))
-    }
-
     render() {
         return (
-            <MuiThemeProvider muiTheme={muiTheme}>
-                <Container>
-                    Hello, world!
-                </Container>
-            </MuiThemeProvider>
+            <Container>
+                Hello, world!
+            </Container>
         )
     }
 }
