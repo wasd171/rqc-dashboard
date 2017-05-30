f5d6fe7d087675bae6152ef9245c6363efe53a34
Konstantin Nesterov <wasd171@gmail.com>
2017-05-30 12:39:49 +0200

    Test Auth0

--- src/renderer/components/App/index.tsx
+++ src/renderer/components/App/index.tsx
@@ -1,9 +1,32 @@
 import * as React from 'react'
+import styled from 'styled-components'
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+
+import Lock from 'renderer/services/Lock'
+
+const muiTheme = getMuiTheme();
+
+const Container = styled.div`
+    min-width: 100vw;
+    max-width: 100vw;
+    min-height: 100vh;
+    max-height: 100vh;
+`
 
 class App extends React.Component<{}, {}> {
+    componentDidMount() {
+        Lock.emailcode((...args) => console.log(args))
+    }
+
     render() {
         return (
-            <div>Hello world!</div>
+            <MuiThemeProvider muiTheme={muiTheme}>
+                <Container>
+                    Hello, world!
+                </Container>
+            </MuiThemeProvider>
         )
     }
 }
---
+++ src/renderer/services/Lock/index.ts
@@ -0,0 +1,8 @@
+import Auth0LockPasswordless from 'auth0-lock-passwordless'
+
+const lock = new Auth0LockPasswordless(
+    'Jqxnc9zU-JNJqmUnFzOh1yCEUHeIv8I8', 
+    'wasd171.eu.auth0.com'
+)
+
+export default lock
