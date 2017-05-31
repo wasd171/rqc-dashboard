27a40477b6ef83367f17b8003cf0a0186c8f71e4
Konstantin Nesterov <wasd171@gmail.com>
2017-05-31 20:08:11 +0200

    Added better login flow

--- src/renderer/components/App/scenes/Auth/index.tsx
+++ src/renderer/components/App/scenes/Auth/index.tsx
@@ -2,13 +2,53 @@ import * as React from 'react'
 import {observable, action, computed} from 'mobx'
 import {observer} from 'mobx-react'
 import {Redirect} from 'react-router-dom'
+import {graphql, ApolloQueryResult, ApolloClient, withApollo} from 'react-apollo'
+import {DocumentNode} from 'graphql'
 
 import authService from 'renderer/services/AuthService'
 
-@observer
-class Auth extends React.Component<undefined, {}> {
-    container: HTMLElement | null = null;
+import * as getUserQuery from './getUserQuery.graphql'
+import * as createUserMutation from './createUserMutation.graphql'
 
+export type QueryOptionsType = {
+    query: DocumentNode
+}
+
+export type QueryResponseType = {
+    viewer: null | {
+        user: null | {
+            id: string
+        }
+    }
+}
+
+export interface IApolloClientWithQueryType extends ApolloClient {
+    query: (options: QueryOptionsType) => Promise<ApolloQueryResult<QueryResponseType>>
+}
+
+export type MutationOptionsType = {
+    variables: {
+        idToken: string
+    }
+}
+
+export type MutationResponseType = {
+    loginUserWithAuth0: {
+        user: {
+            id: string
+        }
+    }
+}
+
+export interface IAuthProps {
+    mutate: (options: MutationOptionsType) => Promise<ApolloQueryResult<MutationResponseType>>
+    client: IApolloClientWithQueryType
+}
+
+@withApollo
+@graphql(createUserMutation)
+@observer
+class Auth extends React.Component<IAuthProps, {}> {
     @observable loggedIn: boolean = false;
 
     @action setLoggedIn = (status: boolean) => {
@@ -24,8 +64,38 @@ class Auth extends React.Component<undefined, {}> {
     }
 
     async componentDidMount() {
-        await authService.logIn();
-        authService.destroyLock();
+        let loggedIn = false;
+
+        while (!loggedIn) {
+            try {
+                const {token} = await authService.logIn();
+
+                const queryRes = await this.props.client.query({
+                    query: getUserQuery
+                })
+                const {viewer} = queryRes.data;
+
+                if (viewer === null || viewer.user === null) {
+                    const mutationRes = await this.props.mutate({
+                        variables: {
+                            idToken: token
+                        }
+                    })
+
+                    authService.saveUserId(mutationRes.data.loginUserWithAuth0.user.id)
+                } else {
+                    authService.saveUserId(viewer.user.id)
+                }
+
+                loggedIn = true
+            } catch (err) {
+                authService.saveToken(null)
+                authService.saveUserId(null)
+            }
+        }
+
+        await authService.closeLock()
+        authService.destroyLock()
         this.setLoggedIn(true)
     }
 
