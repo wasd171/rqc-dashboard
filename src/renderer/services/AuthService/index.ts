20b36900eef582a46a1a35e8bf63f8abee201c31
Konstantin Nesterov <wasd171@gmail.com>
2017-05-31 20:07:13 +0200

    Refactored AuthService (modularized saves)

--- src/renderer/services/AuthService/index.ts
+++ src/renderer/services/AuthService/index.ts
@@ -18,25 +18,19 @@ export class AuthService {
         return this._lock
     }
 
-    logIn() {
+    logIn(): Promise<{token: string}> {
         return new Promise((resolve, reject) => {
             this.lock.emailcode(
-                { autoclose: true, closable: false },
+                { autoclose: false, closable: false },
                 (error: Error | undefined, profile: any, id_token: string | undefined) => {
                     if (error != null) {
                         reject(error)
                     }  else if (id_token == null) {
                         reject(new Error('Received empty id_token from Auth0'))
                     } else {
-                        const {user_id} = profile
-                        localStorage.setItem(ID_TOKEN, id_token)
-                        localStorage.setItem(USER_ID, user_id)
-                        this.token = id_token
-                        this.userId = user_id
-                        console.log({profile})
+                        this.saveToken(id_token)
 
                         resolve({
-                            profile,
                             token: id_token
                         })
                 }
@@ -55,9 +49,31 @@ export class AuthService {
         }
     }
 
+    closeLock() {
+        return new Promise(resolve => this.lock.close(resolve))
+    }
+
     destroyLock() {
         this.lock.destroy();
         this._lock = null;
+    }
+
+    saveToken(token: string | null) {
+        this.token = token
+        if (token !== null) {
+            localStorage.setItem(ID_TOKEN, token)
+        } else {
+            localStorage.removeItem(ID_TOKEN)
+        }
+    }
+
+    saveUserId(userId: string | null) {
+        this.userId = userId;
+        if (userId !== null) {
+            localStorage.setItem(USER_ID, userId)
+        } else {
+            localStorage.removeItem(USER_ID)
+        }
     }
 }
 
